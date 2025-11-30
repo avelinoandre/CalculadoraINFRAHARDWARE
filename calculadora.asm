d8bee36832084135f7ad1275a12004bda207da8f
avelinoandre <andreavelino002@gmail.com>
2025-11-30 18:20:35 -0300

    Implementacao das conversoes Octal, Hexadecimal e BCD.

--- calculadora.asm
+++ calculadora.asm
@@ -3,8 +3,12 @@
 # Revisões/Commits:
 # 
 # 1° commit:
-# Data: 30/11/2025 | Hora: 17:00:00 | Revisao: 1.0
-# Comentario: Estrutura inicial e conversao Decimal -> Binario.
+# Data: 30/11/2025 | Hora: 17:57:00 | Revisao: 1.0
+# Comentário: Estrutura inicial e conversao Decimal -> Binario.
+#
+# 2° commit:
+# Data: 30/11/2025 | Hora: 18:21:00 | Revisao: 2.0 
+# Comentário: Implementacao das conversoes Octal, Hexadecimal e BCD.
 
 .data
     msg_titulo:     .asciiz "CALCULADORA PROGRAMADOR DIDATICA MIPS\n"
@@ -33,6 +37,15 @@ main:
 
     move $a0, $s0
     jal dec_para_bin
+    
+    move $a0, $s0
+    jal dec_para_oct
+    
+    move $a0, $s0
+    jal dec_para_hex
+    
+    move $a0, $s0
+    jal dec_para_bcd
 
     li $v0, 10
     syscall
@@ -48,6 +61,33 @@ dec_para_bin:
     move $t0, $a0
     li $t1, 2
 
+dec_para_oct:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+    li $v0, 4; la $a0, msg_dec_oct; syscall
+    move $t0, $a0; li $t1, 8
+    jal conversao_generica
+    li $v0, 4; la $a0, msg_resultado_oct; syscall
+    jal imprime_resultado
+    lw $ra, 0($sp); addi $sp, $sp, 4; jr $ra
+
+dec_para_hex:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+    li $v0, 4; la $a0, msg_dec_hex; syscall
+    move $t0, $a0; li $t1, 16
+    jal conversao_generica
+    li $v0, 4; la $a0, msg_resultado_hex; syscall
+    jal imprime_hex_resultado
+    lw $ra, 0($sp); addi $sp, $sp, 4; jr $ra
+
+dec_para_bcd:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+    li $v0, 4; la $a0, msg_dec_bcd; syscall
+    li $v0, 4; la $a0, msg_resultado_bcd; syscall
+    lw $ra, 0($sp); addi $sp, $sp, 4; jr $ra
+    
 conversao_loop:
     beq $t0, $zero, fim_loop
 
